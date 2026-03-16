70c259da628ded2a50deaadf3078511f6999f213
RamQ216 <hugomramirez06@gmail.com>
2026-03-16 13:00:29 -0300

    mvp DATOS.py

--- DATOS.py
+++ DATOS.py
@@ -1,1 +1,13 @@
+import json
+import random
 
+def cargar_preguntas():
+    try:
+        with open('preguntas.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            random.shuffle(data) # Mezcla el orden de las preguntas
+            return data
+    except FileNotFoundError:
+        # Preguntas de respaldo por si no encuentran el archivo
+        return [{"pregunta": "Error: preguntas.json no encontrado", 
+                 "opciones": ["A", "B", "C", "D"], "correcta": "A"}]
