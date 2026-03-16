225b2141921f02836759fd30ba573cc8f2e5b61f
Lorena Ramirez <lorenaramirez.py@gmail.com>
2026-03-16 16:47:16 -0300

    se agrega parte cod. para inicio del juego

--- LOGICA.py
+++ LOGICA.py
@@ -1,1 +1,15 @@
+puntos = 0
+vidas = 3
 
+def respuesta (respuesta_usuario, respuesta_correcta):
+    puntos, vidas
+
+    if respuesta_usuario == respuesta_correcta:
+        puntos += 1
+        print("Correcto Ganaste 1 punto.")
+    else:
+        vidas -= 1
+        print("Incorrecto. Pierdes 1 vida.")
+
+    print("Puntos:", puntos)
+    print("Vidas restantes:", vidas)
